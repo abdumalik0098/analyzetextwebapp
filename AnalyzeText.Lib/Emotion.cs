6a57d6ad776c2123a73299c7a4c09d714ec163f7
abdumalik <khidirov.abdumalik@mail.ru>
2022-11-27 17:48:27 +0700

    start files

---
+++ AnalyzeText.Lib/Emotion.cs
@@ -0,0 +1,56 @@
+﻿namespace AnalyzeText.Lib
+{
+    public class Emotion
+    {
+        public string[] Positive = new string[]
+        {
+            "well",
+            "good",
+            "quickly",
+            "happy"
+        };
+        public string[] Negative = new string[]
+        {
+            "unhappy",
+            "sadly",
+            "scary",
+            "bad"
+        };
+
+        public string IsEmotion(string text)
+        {
+            string res = "";
+            int CountPos = 0;
+            int CountNeg = 0;
+            string text2 = text.ToLower();
+
+            foreach (var item in Positive)
+            {
+                if (text2.Contains(item))
+                {
+                    CountPos++;
+                }
+            }
+            foreach (var item in Negative)
+            {
+                if (text2.Contains(item))
+                {
+                    CountNeg++;
+                }
+            }
+            if (CountPos > CountNeg)
+            {
+                res = "POSITIVE";
+            }
+            else if (CountNeg > CountPos)
+            {
+                res = "Negative";
+            }
+            else
+            {
+                res = "Noun";
+            }
+            return res;
+        }
+    }
+}
---
+++ AnalyzeTextWebApp/Controllers/TextController.cs
@@ -0,0 +1,24 @@
+﻿using AnalyzeText.Lib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnalyzeTextWebApp.Controllers
+{
+    public class TextController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EmotionTest()
+        {
+            string text = Request.Form["usertext"];
+            Emotion emotionText = new Emotion();
+            ViewBag.usertext = emotionText.IsEmotion(text);
+
+            return View();
+        }
+    }
+}
